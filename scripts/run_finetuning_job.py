08f45e602822378718a7c0f0d8a22c33e05a4604
ekhadley <ekhadley@gmail.com>
2025-09-02 13:44:22 -0500

    tokenization stuff

--- scripts/run_finetuning_job.py
+++ scripts/run_finetuning_job.py
@@ -68,6 +68,7 @@ Examples:
             f"Loading configuration from {args.config_module} (variable: {args.cfg_var_name})..."
         )
         ft_job = module_utils.get_obj(args.config_module, args.cfg_var_name)
+        print(ft_job)
         assert isinstance(ft_job, FTJob)
 
         dataset = dataset_services.read_dataset(args.dataset_path)
